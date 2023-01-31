be9b36ad671cf52d841cbd39c433a0be06ab2de7
Merik DeVree <c00lr4t@gmail.com>
2023-01-31 03:18:38 -0500

    lots changed, added audio (broken), page content

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var hover = $("#launch_glow1")[0];
+$("#hl_button").mouseenter(function() {
+    hover.play();
+});
+
+var click = $("#wpn_select")[0];
+$("#hl_button").click(function() {
+    click.play();
+});
